ece8264af071df2352da6ca67dfc086eb4491d9a
Pedro de Sant'Anna Novais <psantnovais@gmail.com>
2024-04-17 12:38:49 -0700

    Updated sidebar to properly display text in lg screens. This was needed due to the last update to the sidebar's behavior

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -75,7 +75,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-zinc-800 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-64 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-zinc-800 p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-64'
         }`}
       >
@@ -122,12 +122,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
           <div>
             <h3 className="text-xs uppercase text-amber-300 font-semibold pl-3">
               <span
-                className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6"
-                aria-hidden="true">
-                •••
-              </span>
-              <span
-                className="lg:hidden lg:sidebar-expanded:block 2xl:block">Dashboards</span>
+                className="lg:block lg:sidebar-expanded:block 2xl:block">Dashboards</span>
             </h3>
             <ul className="mt-3">
               {/* Home */}
@@ -144,7 +139,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
                   }`}
                 >
                     <span
-                      className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      className="text-sm font-medium ml-3 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                       Home
                     </span>
                 </NavLink>
@@ -163,7 +158,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
                   }`}
                 >
                       <span
-                        className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                        className="text-sm font-medium ml-3 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                         Current Metrics
                       </span>
                 </NavLink>
@@ -182,7 +177,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
                   }`}
                 >
                   <span
-                    className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Environment</span>
+                    className="text-sm font-medium ml-3 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Environment</span>
                 </NavLink>
               </li>
               {/* System Control */}
@@ -200,7 +195,7 @@ function Sidebar ({ sidebarOpen, setSidebarOpen }) {
                 >
                   <div className="flex items-center justify-between">
                       <span
-                        className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                        className="text-sm font-medium ml-3 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                       System Control
                     </span>
                     {/* Warning icon */}
